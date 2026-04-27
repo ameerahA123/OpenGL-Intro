c66fcef0ff163bfd3b4e254a2bc42c95a6c2c614
ameerahA123 <ameerah.adisa@pau.edu.ng>
2026-04-27 01:49:31 +0100

    Shaders

--- VAO.h
+++ VAO.h
@@ -3,16 +3,16 @@
 
 #include<glad/glad.h>
 #include"VBO.h"
-
 class VAO
 {
 public:
 	GLuint ID;
 	VAO();
 
-	void LinkVBO(VBO& VBO, GLuint layout);
+	void LinkAttrib(VBO& VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
 	void Bind();
 	void Unbind();
 	void Delete();
 };
-#endif#pragma once
+
+#endif
